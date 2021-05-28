0ce67d9983d0d1b72dd8fd76be0535ca35a71419
Suraj Bande <suraj@techmojo.in>
2021-05-28 18:53:22 +0530

    packahe json updated

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/mobilque-exercise'));
+app.use(express.static(__dirname + '/dist/mobilque-exercise'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile('index.html', {root: '/dist/mobilque-exercise/'});
+res.sendFile(path.join(__dirname+'/dist/mobilque-exercise/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
